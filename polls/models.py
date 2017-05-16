19ec88695353cc89d4f61ab6eb925c23cab5cba3
elemeshevsky <elemeshevsky@gmail.com>
2017-05-16 18:01:45 +0300

    5 step added

--- polls/models.py
+++ polls/models.py
@@ -12,7 +12,8 @@ class Question(models.Model):
 	def __str__(self):
 		return self.question_text
 	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		now = timezone.now()
+		return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 @python_2_unicode_compatible
@@ -22,3 +23,5 @@ class Choice(models.Model):
 	votes = models.IntegerField(default=0)
 	def __str__(self):
 		return self.choice_text
+
+
